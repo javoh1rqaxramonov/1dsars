c10aa0a7f29e452b5231ae4a3dc016bc7c75b091
javoh1rqaxramonov <ubotirov22@.com>
2024-07-18 16:02:37 +0500

    finished

---
+++ app.js
@@ -0,0 +1,70 @@
+// 2-masala
+// let a = 3;
+// let b = 6;
+// let res = a+b;
+// console.log(res);
+
+// 3-masala
+// let n = 5;
+// let res =  n * (n - 1) *n
+// console.log(res);
+
+// 4-masala
+// let a = 2 ;
+// let b = 4;
+// let c =5;
+// let res = (a*a)+(b*b)+(c*c);
+// console.log(res);
+
+// 5-masala
+// let a = 4;
+// let b = 4;
+// let res = a**b;
+// console.log(res);
+
+// 7-masala
+// let a = 4;
+// let b = 4**4;
+// let res = b;
+// console.log(res);
+
+
+// 8-masala
+// let a =4;
+// let res = Math.floor(a*10/7);
+// console.log(res);
+
+
+// 1-masala
+// let a=1134;
+// let res = Math.floor(a/100);
+// console.log(res);
+
+// 2-masala
+// let a=1134;
+// let res = Math.floor(a/1000);
+// console.log(res);
+
+// 3-masasa 
+// let a=11334;
+// let res = Math.floor(a/1024);
+// console.log(res);
+
+// 4-masala
+// let a=34;
+// let b =23;
+// let res = Math.floor(a/b);
+// console.log(res);
+
+// 5-masala
+// let a=34;
+// let b =23;
+// let res = res.reverse(a/b);
+// console.log(res);
+
+// 6-masala
+// let a= 23;
+// let res  = Math.trunc(a/10);
+// console.log(res);
+
+
