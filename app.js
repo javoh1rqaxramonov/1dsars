ccca8a90b5bede946355fa69121c8cd0b26ad4a4
javoh1rqaxramonov <ubotirov22@.com>
2024-07-20 19:25:12 +0500

    finished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // 2-masala
 // let a = 3;
 // let b = 6;
@@ -66,5 +67,72 @@
 // let a= 23;
 // let res  = Math.trunc(a/10);
 // console.log(res);
+
+// 7-masala
+// let a =2;
+// let b =3; 
+
+// let sum = parseFloat(a) + parseFloat(b);
+
+
+// console.log(sum);
+
+// 8-masala 
+// let a =12;
+// let res = 2*10+1;
+// console.log(res);
+
+// 9-masala 
+// let a =123;
+// let res = Math.trunc(a/100);
+// console.log(res);
+
+// 10-masala 
+// let a =123;
+// let res = a%10;
+// let sum = Math.trunc(a/10)%10;
+// console.log(res);
+// console.log(sum);
+
+// 11-masala
+// let a =2;
+// let b =3; 
+// let c=4;
+// let sum = parseFloat(a) + parseFloat(b) + parseFloat(c);
+// console.log(sum);
+
+// 12-masala
+// let a = 123;
+// let res = a%10;
+// let sum = Math.trunc(a/10)%10;
+// let b = Math.trunc(a/100);
+// console.log(res);
+// console.log(sum);
+// console.log(b);
+
+// 1-savol 
+// 1- node js 
+// 2-consoledan
+
+// 2-savol 
+// bitta idishdagi qiymat
+
+// 3-savol 
+// const,var,let 
+
+// 4-savol 
+// numberOfRooms
+// NumberOfRoom
+// number_of_room
+// NUMBER_OF_ROOMS
+ 
+// 7-savol 
+// 'Hi there 👋🏽 \nMy name is ' + name + ' and I am ' + age + ' years old.'
+// (Math.random() * n1 + n2)
+
+// 8-savol 
+// Ko'p qatorli Izoh/*
+
+
 
 
